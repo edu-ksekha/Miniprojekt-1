ce09863d39c1b8ff99aa6b732d0389e12c9da71d
Kseniya Khafizovanagornykh <kseniya.khafizovanagornykh@skola.taby.se>
2026-04-21 07:37:30 +0000

    Skapade meny

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # main.py
+
+def meny():
+    print("1. Visa alla citat")
+    print("2. Lägg till nytt citat")
+    print("3. Visa slumpmassigt citat")
+    print("4. Avsluta")
+
+def main(lista):
+    while True:
+        meny()
+        val = input("Välj:")
